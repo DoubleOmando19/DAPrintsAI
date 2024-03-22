a5c617da921e97dc58885845815f4ad120ec165e
HenryA <henry_alcaide@hotmail.com>
2024-03-22 13:12:00 -0400

    update amazon.js and amazon css

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,21 +27,10 @@ products.forEach((product) => {
       </div>
       
       <div class="product-quantity-container">
-        <select>
-          <option selected value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
+        <p> Digital Download
+        </p>
       </div> 
-
+      
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
